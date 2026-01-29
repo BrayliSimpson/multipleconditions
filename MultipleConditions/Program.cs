c37a4c4587b6b71300f84e9f1b4d014313c6b102
BraylSimpson <Brayli.R.Simpson-1@ou.edu>
2026-01-29 12:53:42 -0600

    FIrst GitHub repo code for else and multiple conditions

--- MultipleConditions/Program.cs
+++ MultipleConditions/Program.cs
@@ -1,27 +1,34 @@
-﻿Console.WriteLine("PLease enter membership type - Gold, Silver, or Guest:")
+﻿Console.WriteLine("Please enter yourn membership type - gold, silver, or guest:");
+string membership = Console.ReadLine();
 
-    if (membership == "Gold")
+if (membership == "Gold")
 {
-    Console.WriteLine("Welcome, Gold Member!")
+    Console.WriteLine("Welcome, Gold Member !");
 }
-    else if (membership == "Silver")
+else if (membership == "Silver")
 {
-    Console.WriteLine("Welcome, Silver Member!")
-}
-else (membership == "Guest")
-        {
-    Console.WriteLine("Welcome, Guest!")
+    Console.WriteLine("Welcome, Silver Member !");
 }
 
-string message = membership = "Gold" ? "Welcome, Gold Member!":
-                membership = "silver" ? "Welxome, Silver memeber":
-                membership = "guest" ? "welcome, guest!";
+else
+{
+    Console.WriteLine("Welcome, Guest !");
+}
 
-double discount == membership == "Gold" ? 0.20 :
-                    membership =="Silver" ? 0.10 :
-                    0.0;
-Console.WriteLine($"Your discount rate: {discount:P2}");
+//using terney operatior to handle mutiple conditons
+string message = membership == "Gold" ? "Welcome, Gold Member !" :
+                 membership == "Silver" ? "Welcome, Silver Member !" :
+                 "Welcome Guest!";
 
-double pre_discount_rate = 100.0
-double_final_price = pre_discount_rate* (1-discount);
-Console.WriteLine($"The final price is : {final_price:F2}")
+Console.WriteLine(message);
+
+double dicount = membership == "Gold" ? 0.20 :
+                  membership == "Silver" ? 0.10 :
+                  0.0;
+
+Console.WriteLine($"Your discount rate : {dicount:P2}");
+
+double pre_discount_price = 100.0;
+double final_price = pre_discount_price * (1 - dicount);
+Console.WriteLine($"Your final price is: {final_price:F2}");
+
